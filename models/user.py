d482a986fc4cdaa8f6481364814c9696b77c0f0f
anozie5 <78419937+anozie5@users.noreply.github.com>
2023-06-04 20:06:44 +0100

    My commit

--- models/user.py
+++ models/user.py
@@ -1,19 +1,49 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""
+User Class from Models Module
+"""
+import hashlib
+import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=True, default="NULL")
-    last_name = Column('last_name', String(128), nullable=True, default="NULL")
-    # backref may need to be back_populates?Below line commented out bc console
-    # would not run with it in. This line was implemented in Task 8
-    places = relationship("Place", cascade="delete", backref="user")
-    # Below line is commented out for caution and was added in Task 9
-    reviews = relationship("Review", cascade="delete", backref="user")
+    """
+        User class handles all application users
+    """
+    if STORAGE_TYPE == "db":
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+        places = relationship('Place', backref='user', cascade='delete')
+        reviews = relationship('Review', backref='user', cascade='delete')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        """
+            instantiates user object
+        """
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                User.__set_password(self, pwd)
+        super().__init__(*args, **kwargs)
+
+    def __set_password(self, pwd):
+        """
+            custom setter: encrypts password to MD5
+        """
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        secure_password = secure.hexdigest()
+        setattr(self, "password", secure_password)
